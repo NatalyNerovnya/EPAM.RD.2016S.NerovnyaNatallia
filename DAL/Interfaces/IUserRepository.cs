93b39a18e4b8a63e5862e8bc6f88a33a85a57c94
Nataly Nerovnya <natanerovnja@gmail.com>
2016-07-21 19:53:48 +0300

    Add project files.

---
+++ DAL/Interfaces/IUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUserRepository
+    {
+        int Add(User user);
+
+        void Delete(User user);
+
+        IEnumerable<User> GetAll();
+
+        int[] SearchForUsers(Func<User, bool> criteria);
+
+        void Save();
+
+        void Load();
+    }
+}
---
+++ DAL/Interfaces/IUserStorage.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IUserStorage
+    {
+        List<User> Users { get; set; }
+        IUserIdIterator IdIterator { get; }
+        IUserValidator Validator { get; }
+        int Add(User user);
+        void Delete(User user);
+        IEnumerable<User> GetAll();
+        int[] SearchFor(Func<User, bool> criteria);
+        void Save();
+        void Load();
+    }
+}
